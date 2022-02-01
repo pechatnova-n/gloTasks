a210de638e6614591ece14b5f6e72c066b0478dd
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-02-02 01:58:27 +0300

    task lesson04

--- scripts.js
+++ scripts.js
@@ -1,49 +1,19 @@
-let num =  266219;
 
-let arr = num.toString().split("");
-console.log(arr);
-
-let multi = 1;
-for (let i = 0; i < arr.length; i++) {
-    multi *= +arr[i];
-}
-console.log(multi);
-
-multi = multi ** 3;
-console.log(multi.toString().split("").slice(0,2).join(""));
-
-
-
-const lang = 'ru';
-
-if (lang == 'ru') {
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-} if (lang == 'en') {
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+function find(good) {
+    if(typeof(good) != 'string') {
+        console.log("Это не строка");
+    } else {
+        good = good.trim();
+        if(good.length > 30) {
+            let str = good.slice(0, 30);
+            console.log(str + '...');
+        } else {
+            console.log(good);
+        }
+    }
 }
 
-
-switch (lang) {
-    case 'ru':
-        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-        break;
-    case 'en':
-        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-        break;
-}
-
-
-const days = {
-    ru: ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'],
-    en:  ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']
- }
-console.log(days[lang]);
-
-
-const namePerson = 'Александр';
-const nameCheck = (namePerson == 'Артем') ? console.log('директор'):
-    (namePerson == 'Александр') ? console.log('преподаватель') : console.log('студент');
-
+find('   Мы ели-ели ершей у ели; их еле-еле у ели доели  ');
 
 
 
