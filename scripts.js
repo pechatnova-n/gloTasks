eda48a98247d14414cfc6b5ecfdc4673eb3846b8
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-02-06 12:28:55 +0300

    fix lesson07

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
 'use strict';
 
 const block = document.getElementById("block");
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const week = ['Воскресенье','Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 const day = new Date();
 
 
 week.forEach(function(item) {
     if(item == 'Суббота' || item == 'Воскресенье') {
-        block.innerHTML += item.italics() + '</br>';
-    } else if(week[day.getDay() -1] == item) {
-        block.innerHTML += `<b>${item}</b></br>`;
+        if (week[day.getDay()] == item) {
+            block.innerHTML += `<b>${item.italics()}</b></br>`;
+        } else {
+            block.innerHTML += `${item.italics()}</br>`;
+        }
+    } else if(week[day.getDay()] == item) {
+         block.innerHTML += `<b>${item}</b></br>`;
     } else {
         block.innerHTML += item + '</br>';
     }
